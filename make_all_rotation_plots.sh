88f3c5155caa3cfa0a8cbe30f6835ccd85a4093c
Sam Vaughan <sam.vaughan@sydney.edu.au>
2022-01-13 17:20:16 +1100

    deleted unneeded files

--- make_all_rotation_plots.sh
+++
@@ -1,9 +0,0 @@
-python3 jvds_display.py 1 13
-python3 jvds_display.py 1 14
-python3 jvds_display.py 1 18
-python3 jvds_display.py 1 21
-
-python3 jvds_display.py 3 13
-python3 jvds_display.py 3 14
-python3 jvds_display.py 3 18
-python3 jvds_display.py 3 21
